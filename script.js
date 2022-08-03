da595c5c4e198327ea1e23869d0b773e44bf4754
Roman Geathers <romangeathersdev@gmail.com>
2022-08-03 13:03:48 -0400

    Fix password match bugs and add warning message
    Add a message informing user that passwords do not match if they don't.
    Fix password match function so that it is updated consistently before
    user clicks create account button. This way they will know real-time
    whether or not their passwords match.

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 function compare_passwords() {
-  const pass_input = document.querySelector("#password");
-  const pass_confirm_input = document.querySelector("#password-confirm");
-
   if (pass_input.value !== "" && pass_confirm_input.value !== "") {
     if (pass_input.value !== pass_confirm_input.value) {
+      pass_input.classList.remove("match");
+      pass_confirm_input.classList.remove("match");
       pass_input.classList.add("error");
       pass_confirm_input.classList.add("error");
+      password_fields.appendChild(no_match);
     }
     else {
+      pass_input.classList.remove("error");
+      pass_confirm_input.classList.remove("error");
       pass_input.classList.add("match");
       pass_confirm_input.classList.add("match");
+      password_fields.removeChild(no_match);
     }
   }
 }
 
-const submit_btn = document.querySelector(".create-acct-btn");
-let no_match = document.createElement("p");
+const pass_input = document.querySelector("#password");
+const pass_confirm_input = document.querySelector("#password-confirm");
+const password_fields = document.querySelector(".input-fields");
 
-submit_btn.addEventListener('click', () => {
+// const submit_btn = document.querySelector(".create-acct-btn");
+const no_match = document.createElement("p");
+const text = document.createTextNode("* Passwords do not match");
+no_match.appendChild(text);
+no_match.classList.add("no-match-text");
+no_match.style.fontSize = "0.8rem"
+
+
+// submit_btn.addEventListener("click", () => {
+//   compare_passwords();
+// });
+
+pass_input.addEventListener("keyup", () => {
   compare_passwords();
-});
+})
 
+pass_confirm_input.addEventListener("keyup", () => {
+  compare_passwords();
+})
